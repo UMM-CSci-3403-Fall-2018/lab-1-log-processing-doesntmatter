32a65960eb3bf476bbf0569cccebca9227e1b648
HuntsmanRoym <grins008@morris.umn.edu>
2018-09-11 22:06:42 -0500

    add wrap_contents.sh to bin (in tmp directory) and finish(?) code cleanlieness in create_username_dist.sh

--- tmp/bin/create_username_dist.sh
+++ tmp/bin/create_username_dist.sh
@@ -6,4 +6,6 @@ targetDirectory=$1
 cd ..
 
 #Gather required contents of failed_login_data
-cat log_files/$targetDirectory/failed_login_data.txt | awk '{ print $4}' | sort | uniq -c | awk '{ print "data.addrow([\x27"$2"\x27, "$1"])"}' > tmptxt.tx
+cat log_files/$targetDirectory/failed_login_data.txt | awk '{ print $4}' | sort | uniq -c | awk '{ print "data.addrow([\x27"$2"\x27, "$1"])"}' > tmptxt.txt
+
+
