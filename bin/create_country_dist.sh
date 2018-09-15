47dd58c26754e998c8f5df5011e689bca19db0c1
HuntsmanRoym <grins008@morris.umn.edu>
2018-09-15 16:41:58 -0500

    Made it so each country code was added together based on the same country code ('Us 29') as opposed to having something like: 'Us 5' and then 'Us 24' below it

--- bin/create_country_dist.sh
+++ bin/create_country_dist.sh
@@ -7,7 +7,10 @@ cat $targetDirectory/*/failed_login_data.txt | awk '{ print $5}' | \
 	join -1 1 -2 1 etc/country_IP_map.txt - | \
 	awk ' { print $2}' | \
 	uniq -c | \
-	awk '{ print "data.addRow([\x27"$2"\x27, "$1"]);"}' > \
+	awk '{ print "data.addRow([\x27"$2"\x27, "$1"]);"}' | \
+	awk '{arr[$1]+=$2} END {for (i in arr) {print i , arr[i]}}' | \
+	awk '{ print $1, $2"]);"}' | \
+	sort > \
 	$targetDirectory/tmptxt3.txt 
 
 bin/wrap_contents.sh $targetDirectory/tmptxt3.txt html_components/country_dist $targetDirectory/country_dist.html
