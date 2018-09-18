dfa3eddd0892ab1b2dd3c69ade5be3e1bb97d9b5
HuntsmanRoym <grins008@morris.umn.edu>
2018-09-18 18:35:37 -0500

    Got all but final test to pass. I found out that the issue lies with not taking in all of the data, but I don't really have the time to spend on trying to fix it - Jacob

--- bin/process_logs.sh
+++ bin/process_logs.sh
@@ -4,7 +4,6 @@ originalLocation=$(pwd)
 
 #Makes a temp directory --- -d gives random name to the temp directory, I think - Needs to be deleted manually (like how we deleted tmptxt files)
 NEW_TEMP_DIR=`mktemp -d`
-echo $NEW_TEMP_DIR
 cd $NEW_TEMP_DIR 
 mkdir log_files
 cd $originalLocation
@@ -21,7 +20,7 @@ bin/create_hours_dist.sh $NEW_TEMP_DIR
 
 bin/create_country_dist.sh $NEW_TEMP_DIR
 
-bin/assemble_reports.sh $NEW_TEMP_DIR
+bin/assemble_report.sh $NEW_TEMP_DIR
 
 mv $NEW_TEMP_DIR/failed_login_summary.html $originalLocation
 
